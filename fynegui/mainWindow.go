94477d63fc4ca19c778e5e54664ab64839b20d26
smaTc <m.fitzke@gmx.net>
2020-04-02 18:12:03 +0200

    added menu bar

--- fynegui/mainWindow.go
+++ fynegui/mainWindow.go
@@ -20,16 +20,51 @@ func Run() {
 	mainWindow = rpd.NewWindow("Remote Play Detached")
 	mainWindow.Resize(fyne.NewSize(600, 350))
 	mainWindow.SetContent(buildMainContent())
+	mainWindow.SetMaster()
 	mainWindow.ShowAndRun()
 }
 
 func buildMainContent() *fyne.Container {
+	mainWindow.SetMainMenu(buildMainMenu())
 	mainContainer = fyne.NewContainerWithLayout(layout.NewVBoxLayout(), buildAppListContainer(), layout.NewSpacer(), buildButtonBar())
 	return mainContainer
 }
 
 func refreshContent() {
 	mainWindow.SetContent(buildMainContent())
+}
+
+func buildMainMenu() *fyne.MainMenu {
+
+	mainMenu := fyne.NewMainMenu(fyne.NewMenu("Menu",
+		fyne.NewMenuItem("About", func() {
+			fmt.Println("clicked: About")
+			aboutWindow := rpd.NewWindow("About")
+			aboutWindow.Resize(fyne.NewSize(500, 400))
+
+			licenseLabel := widget.NewLabel(LICENSE)
+			licenseLabel.Resize(fyne.NewSize(450, 350))
+
+			scrollContainer := widget.NewScrollContainer(licenseLabel)
+			//scrollContainer.Resize(fyne.NewSize(450, 350))
+			//vbox := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), scrollContainer)
+
+			okButton := widget.NewButton("OK", func() {
+
+				aboutWindow.Close()
+
+				refreshContent()
+			})
+
+			buttons := fyne.NewContainerWithLayout(layout.NewHBoxLayout(), okButton)
+
+			aboutWindow.SetContent(fyne.NewContainerWithLayout(layout.NewVBoxLayout(), scrollContainer, layout.NewSpacer(), buttons))
+
+			aboutWindow.Show()
+
+		}),
+	))
+	return mainMenu
 }
 
 func buildButtonBar() *fyne.Container {
